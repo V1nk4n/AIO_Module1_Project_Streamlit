a9b68d2063fd6a3b6a220312cf5859013e909384
V1nk4n <vinhan188@gmail.com>
2024-06-30 20:36:54 +0700

    Update sonar

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -1,42 +1,44 @@
 import streamlit as st
 
-def levenshtein_distance(token1, token2):
-    distances = [[0]*(len(token2)+1) for i in range(len(token1)+1)]
 
-    for t1 in range(len(token1) + 1):
+def init_distances(len1, len2):
+    distances = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+    for t1 in range(len1 + 1):
         distances[t1][0] = t1
-
-    for t2 in range(len(token2) + 1):
+    for t2 in range(len2 + 1):
         distances[0][t2] = t2
+    return distances
 
-    a = 0
-    b = 0
-    c = 0
 
-    for t1 in range(1, len(token1) + 1):
-        for t2 in range(1, len(token2) + 1):
-            if (token1[t1-1] == token2[t2-1]):
+def compute_cost(token1, token2, distances):
+    len1, len2 = len(token1), len(token2)
+    for t1 in range(1, len1 + 1):
+        for t2 in range(1, len2 + 1):
+            if token1[t1 - 1] == token2[t2 - 1]:
                 distances[t1][t2] = distances[t1 - 1][t2 - 1]
             else:
-                a = distances[t1][t2 - 1]
-                b = distances[t1 - 1][t2]
-                c = distances[t1 - 1][t2 - 1]
+                insert = distances[t1][t2 - 1] + 1
+                delete = distances[t1 - 1][t2] + 1
+                replace = distances[t1 - 1][t2 - 1] + 1
+                distances[t1][t2] = min(insert, delete, replace)
+    return distances
 
-                if (a <= b and a <= c):
-                    distances[t1][t2] = a + 1
-                elif (b <= a and b <= c):
-                    distances[t1][t2] = b + 1
-                else:
-                    distances[t1][t2] = c + 1
 
+def levenshtein_distance(token1, token2):
+    distances = init_distances(len(token1), len(token2))
+    distances = compute_cost(token1, token2, distances)
     return distances[len(token1)][len(token2)]
+
 
 def load_vocab(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
     words = sorted(set([line.strip().lower() for line in lines]))
     return words
+
+
 vocabs = load_vocab(file_path='./vocab.txt')
+
 
 def main():
     st.title("Word Correction using Levenshtein Distance")
@@ -48,18 +50,20 @@ def main():
         leven_distances = dict()
         for vocab in vocabs:
             leven_distances[vocab] = levenshtein_distance(word, vocab)
-        
+
         # sorted by distance
-        sorted_distences = dict(sorted(leven_distances.items(), key=lambda item: item[1]))
+        sorted_distences = dict(
+            sorted(leven_distances.items(), key=lambda item: item[1]))
         correct_word = list(sorted_distences.keys())[0]
         st.write('Correct word: ', correct_word)
 
         col1, col2 = st.columns(2)
         col1.write('Vocabulary:')
         col1.write(vocabs)
-        
+
         col2.write('Distances:')
         col2.write(sorted_distences)
+
 
 if __name__ == "__main__":
     main()
